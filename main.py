41519b669b9b86313b68dc64e537f204eb1de6e1
mint_mf <froars@yandex.ru>
2024-03-05 16:42:18 +0300

    FastAPI test

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import json
 from fastapi import FastAPI
+import urllib.request
 
 app = FastAPI()
 
@@ -6,3 +8,21 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/page2")
+async def root():
+    return {"message": "Page two"}
+
+@app.get("/weather")
+async def root():
+    url = "http://ipinfo.io/json"
+    response = urllib.request.urlopen(url)
+    data = json.load(response)
+
+    IP = data["ip"]
+    org = data["org"]
+    city = data["city"]
+    country = data["country"]
+    region = data["region"]
+
+    return {"message": IP}
